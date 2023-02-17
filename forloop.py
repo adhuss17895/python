ff09a6b87772e9048e0d5245bb6d81f8ef15185d
adhuss17895 <adhir.17895@gmail.com>
2023-02-17 13:20:27 +0530

    FEB17

---
+++ forloop.py
@@ -0,0 +1,9 @@
+ #SHOPPING CART
+
+prices = [10,20,30]
+total_cost = 0
+
+for price in prices:
+    total_cost += price
+
+print(total_cost)
---
+++ largest_no_list.py
@@ -0,0 +1,9 @@
+numbers = [15,98,156,4,789]
+max = numbers[0]
+
+for number in numbers:
+    if number > max:
+        max = number
+
+
+print(f'Largest number is: {max}')
